b5170dae950b0fa075da9f7f3da4d5c9fca46302
Rehan <47797856+rsa16@users.noreply.github.com>
2022-03-03 14:25:57 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-bold text-4xl text-center">"All good things come to end"</h1>
+    </main>
+  );
+}
